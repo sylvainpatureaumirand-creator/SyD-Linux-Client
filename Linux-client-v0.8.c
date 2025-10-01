18f2bc0c2c7fd1978749383985e3ed33aed1a86b
Sylvain PM <sylvain.patureau.mirand@gmail.com>
2025-10-01 09:43:28 +0200

    Update Linux-client-v0.8.c
    Ajout licence gpl-v3

--- Linux-client-v0.8.c
+++ Linux-client-v0.8.c
@@ -1,5 +1,22 @@
 /*
+ * Copyright (C) 2025 SylPaMir <sylvain.patureau.mirand@gmail.com>
+ * This file is part of SyD Project.
+ *
+ * SyD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SyD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SyD.  If not, see <https://www.gnu.org/licenses/>.
+*/ 
 
+/*
 #Installer les librairies et le compilateur
 sudo apt update
 sudo apt install build-essential libssl-dev libgmp-dev
@@ -2623,5 +2640,6 @@ int main(int argc, char *argv[]) {
     
   return 0;  
 }
+
 
 
